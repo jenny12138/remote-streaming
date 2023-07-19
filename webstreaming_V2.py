6a9b75ab1441e9c6a0f2e6140a22b5f4470e6e79
jenny12138 <59715785+jenny12138@users.noreply.github.com>
2023-07-19 15:35:30 -0400

    Update webstreaming_V2.py

--- webstreaming_V2.py
+++ webstreaming_V2.py
@@ -1,5 +1,6 @@
 """
 Necessary imports
+On the Jetson Nano, I was using Python version 3.6.9, Numpy version 1.13.3, and cv2 version 4.1.1
 """
 import platform # to determine which version of Python is being run
 print("This script is run using Python interpreter", platform.python_version()) 
@@ -13,17 +14,14 @@ print("This script is using Numpy version", np.__version__)
 from flask import Response, Flask, render_template
 import numpy
 import cv2
-import imutils
-from imutils.video import VideoStream
 print("This script is using cv2 version", cv2.__version__)
 
 """
 Performing some initializations
 """
-outputFrame = None
 lock = threading.Lock() # to prevent race condition; in this case, it ensures that one thread isn't trying to read the frame as it is being updated.
 app = Flask(__name__) # initialize a flask object
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0) # so that we can use the webcam
 time.sleep(5.0) # wait for the camera to warm up. Changed to 5 seconds as it looks like it takes a while for the camera to turn on.
 
 """
@@ -35,13 +33,17 @@ def index():
 
 """
 Function to return a frame
+
+@Alex: I was thinking we can run your model in here, as all this function is doing right now is reading in a frame from the camera, then transforming the output to byte format.
+So, depending on whether your model does things frame by frame, maybe we can just add a step in between where we feed the current frame into your model, get an output frame, 
+and then transform that output frame into byte format?
 """
 def return_frame():
     # grab global references to the video stream, output frame, and lock variables.
-    global camera, outputFrame, lock
+    global camera, lock
 
     while True:
-        with lock: # wait until the lock is acquired
+        with lock: # wait until the lock is acquired. We need to acquire the lock to ensure the frame variable is not accidentally being read by a client while we are trying to update it.
             success, frame = camera.read() # read the camera frame
             if not success:
                 break
@@ -51,8 +53,6 @@ def return_frame():
                     continue
 
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n') # yield some text and the output frame in the byte format
-        # the yield keyword will turn any expression that is given with it into a generator object and return it to the caller. Therefore, you must
-        # iterate over the genertor object if you wish to obtain the values stored there.
 
 """
 Function to use the flask function Response
@@ -70,14 +70,14 @@ if __name__ == '__main__':
     ap.add_argument("-o", "--port", type=int, required=True, help="ephemeral port number of the server (1024 to 65535)")
     args=vars(ap.parse_args())
 
-    # start a thread
+    # start a thread that will perform motion detection
     t = threading.Thread(target=return_frame)
     t.daemon = True
     t.start()
 
     # start the flask app
-    #app.run(host=args["ip"], port=args["port"], debug=True, threaded=True, use_reloader=False)
-    app.run(host='10.42.0.1', port=args["port"], debug=True, threaded=True, use_reloader=False)
+    app.run(host='10.42.0.1', port=args["port"], debug=True, threaded=True, use_reloader=False) # Find the IP address of the Jetson device manually, then replace host=... with that ip address
+    # to find the ip address, use the ifconfig command. Under wlan, the ip address is listed after the "inet" field.
 
 camera.release() # Before ending the script release the camera.
 print("End of script.")
